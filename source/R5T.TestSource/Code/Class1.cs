7e2776184fae5429098282a943f288147b5cb81f
MinexAutomation <minex.automation@gmail.com>
2019-06-05 11:41:18 -0400

    Added MessageSource class.

--- source/R5T.TestSource/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.TestSource
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.TestSource/Code/Classes/MessageSource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.TestSource
+{
+    public class MessageSource
+    {
+        public static string Message = "This is the first message.";
+    }
+}
